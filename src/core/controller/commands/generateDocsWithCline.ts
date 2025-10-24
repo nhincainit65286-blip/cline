f283d8335b8f276dd34ea253cb57d4745eaf9c5b
nhincainit65286-blip <nhincainit65286@gmail.com>
2025-10-24 21:08:24 +0700

    Refactor: improve prompt formatting for generating docs

--- src/core/controller/commands/generateDocsWithCline.ts
+++ src/core/controller/commands/generateDocsWithCline.ts
@@ -4,10 +4,6 @@ import { CommandContext } from "@/hosts/vscode/commandUtils";
 export async function generateDocsWithCline(controller: Controller, context: CommandContext) {
     const { selection } = context;
     const fileMention = selection.fileMention;
-    const prompt = `Generate documentation for the following code from ${fileMention} in Vietnamese:
-
-`${selection.language}
-${selection.content}
-````;
+    const prompt = `Generate documentation for the following code from ${fileMention} in Vietnamese:\n\n\`\`\`${selection.language}\n${selection.content}\n\`\`\``;
     await controller.initTask(prompt);
 }
